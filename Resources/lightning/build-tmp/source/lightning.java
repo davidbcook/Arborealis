2aa28e0b12c91b323f5b814b442f681795c74ffb
jpark <jypark03@gmail.com>
2015-07-22 19:43:43 -0700

    <lightning works>

--- Resources/lightning/build-tmp/source/lightning.java
+++ Resources/lightning/build-tmp/source/lightning.java
@@ -1,15 +1,38 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import com.heroicrobot.dropbit.registry.*; 
+import com.heroicrobot.dropbit.devices.pixelpusher.Pixel; 
+import com.heroicrobot.dropbit.devices.pixelpusher.Strip; 
+import java.util.*; 
+import ddf.minim.*; 
+import ddf.minim.analysis.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class lightning extends PApplet {
+
 /*
 * This file makes the pixels imitate rain. When it receives a sound 
 * input, it flashes white like lightning. 
 */
 
-import com.heroicrobot.dropbit.registry.*;
-import com.heroicrobot.dropbit.devices.pixelpusher.Pixel;
-import com.heroicrobot.dropbit.devices.pixelpusher.Strip;
-import java.util.*;
 
-import ddf.minim.*;
-import ddf.minim.analysis.*;
+
+
+
+
+
+
 
 TestObserver testObserver;
 DeviceRegistry registry;
@@ -19,7 +42,7 @@ AudioInput drumbeat;
 float drumSensitivity = 5; // Adjust for sensitivity
 int rainFreq = 0; // Adjust for frequency of rain
 
-void setup() {
+public void setup() {
   colorMode(RGB);
 
   registry = new DeviceRegistry();
@@ -33,7 +56,7 @@ void setup() {
   
 }
 
-void draw() {
+public void draw() {
  
  if(testObserver.hasStrips) {
    //pixelpusher code is wrapped in here
@@ -54,10 +77,10 @@ void draw() {
    if(drumbeat.mix.level() * 100 > drumSensitivity) { 
      
      for (int p = 0; p < strip.getLength(); p++) {
-       strip.setPixel(color(#ffffff), p); //lightning
+       strip.setPixel(color(0xffffffff), p); //lightning
      }    
      
-     // if you want to play with a traveling pixel 
+     // if you want to play with a traveling 
      // for (int p = 0; p < strip.getLength(); p++) {
      // 	int idletime = millis() + 2;
      // 	while (idletime>millis()) {
@@ -68,9 +91,28 @@ void draw() {
    }
  } 
 
-void stop() {
+public void stop() {
   //always close Minim audio classes when you are done with them
   drumbeat.close();
   minim.stop();
   super.stop(); 
+}
+class TestObserver implements Observer {
+  public boolean hasStrips = false;
+  public void update(Observable registry, Object updatedDevice) {
+    println("Registry changed!");
+    if (updatedDevice !=null) {
+      println("Device change: " + updatedDevice);
+    }
+    this.hasStrips=true;
+  }
+};
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
